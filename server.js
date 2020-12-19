462eb28e7ef22c80673ec8462a8769577656bffa
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-12-19 10:20:10 -0800

    Finished project

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const cors = require('cors');
 const app = express();
 const urlExist = require('url-exist');
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-
-// Database Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
